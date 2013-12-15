e54d0059081667b4b07ff7b5b5365f68b99a2cf0
Will McCutchen <will@mccutch.org>
2013-12-15 17:06:35 -0500

    Initial commit

---
+++ markovangelo.py
@@ -0,0 +1,29 @@
+
+import Image
+import vokram
+
+
+def main():
+    img = Image.open('monalisa.gif')
+    w, h = img.size
+    pix = img.load()
+    tokens = tokenize(w, h, pix)
+    model = vokram.build_model(tokens)
+
+    new_pix = vokram.markov_chain(model, w * h)
+    new_pix_iter = iter(new_pix)
+    for y in range(h):
+        for x in range(w):
+            pix[x, y] = next(new_pix_iter)
+
+    img.show()
+
+
+def tokenize(w, h, pix):
+    for y in range(h):
+        for x in range(w):
+            yield pix[x, y]
+
+
+if __name__ == '__main__':
+    main()
